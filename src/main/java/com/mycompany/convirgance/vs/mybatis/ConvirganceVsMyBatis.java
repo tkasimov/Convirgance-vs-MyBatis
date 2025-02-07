98bdca9733aa48933af915d40a30f4764814f35e
Timur Kasimov <timurkasimov@invirgance.com>
2025-02-07 14:28:51 -0600

    First commit

---
+++ src/main/java/com/mycompany/convirgance/vs/mybatis/ConvirganceVsMyBatis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.convirgance.vs.mybatis;
+
+/**
+ *
+ * @author timur
+ */
+public class ConvirganceVsMyBatis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
