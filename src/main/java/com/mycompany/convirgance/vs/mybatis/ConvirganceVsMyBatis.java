6cf8bffa2d537a85d439c920c0680f8498e7725f
Timur Kasimov <timurkasimov@invirgance.com>
2025-02-07 17:21:10 -0600

    started working on mybatis example, set up config xml file and started the mapper xml file

--- src/main/java/com/mycompany/convirgance/vs/mybatis/ConvirganceVsMyBatis.java
+++ src/main/java/com/mycompany/convirgance/vs/mybatis/ConvirganceVsMyBatis.java
@@ -4,13 +4,22 @@
 
 package com.mycompany.convirgance.vs.mybatis;
 
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 /**
  *
  * @author timur
  */
-public class ConvirganceVsMyBatis {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class ConvirganceVsMyBatis 
+{
+    public static void main(String[] args) throws IOException
+    {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory session = new SqlSessionFactoryBuilder().build(inputStream);
     }
 }
