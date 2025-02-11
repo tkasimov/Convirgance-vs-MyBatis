d1ed4325dd9994e19377d9bb30aed967f2b06ad5
Timur Kasimov <timurkasimov@invirgance.com>
2025-02-10 22:18:42 -0600

    export to csv

--- src/main/java/com/mycompany/convirgance/vs/mybatis/ConvirganceVsMyBatis.java
+++ src/main/java/com/mycompany/convirgance/vs/mybatis/ConvirganceVsMyBatis.java
@@ -4,20 +4,16 @@
 
 package com.mycompany.convirgance.vs.mybatis;
 
-
-
-
+import java.io.FileWriter;
 import java.util.List;
 import java.io.IOException;
 import java.io.InputStream;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.*;
-
-
-
 
 
 /**
@@ -28,13 +24,47 @@ public class ConvirganceVsMyBatis
 {
     public static void main(String[] args) throws IOException
     {
-        // get SQLSessionFacotyr
+        // get SQLSessionFactory
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        
+        // create a session
         SqlSession session = sessionFactory.openSession();
-//        
+        // record customers
         List<Customer> customers = session.selectList("com.mycompany.convirgance.vs.mybatis.CustomerMapper.getAll");
+        
+        
+        //export to CSV
+        CSVPrinter printer = new CSVPrinter(new FileWriter("customer.csv"), CSVFormat.DEFAULT);
+        // header
+        printer.printRecord("Customer_id", "Discount_code", "Zip", "Name", 
+        "Addressline1", "Addressline2", "City", "State", "Phone", "Fax", 
+        "Email", "Credit_limit");
+        
+        for (Customer customer : customers)
+        {
+            Object[] data = 
+            {
+                String.valueOf(customer.getCustomer_id()), 
+                customer.getDiscount_code(), 
+                customer.getName(), 
+                customer.getAddressline1(),
+                customer.getAddressline2(),
+                customer.getCity(),
+                customer.getState(),
+                customer.getPhone(),
+                customer.getFax(), 
+                customer.getEmail(),
+                customer.getCredit_limit() 
+            };
+            printer.printRecord(data);   
+        }
+        printer.flush();
+        
+        //export to JSON
+        
+        
+        // close a session
+        session.close();
     }
 }
